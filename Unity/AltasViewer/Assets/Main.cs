314d9d9c5aa94c603570fc9fb7c0e898524c2740
peter <hofstedemail@yahoo.com>
2015-10-04 01:30:06 +0200

    passing json to copypixels, start of cleanup

--- Unity/AltasViewer/Assets/Main.cs
+++ Unity/AltasViewer/Assets/Main.cs
@@ -21,14 +21,14 @@ public class Main : MonoBehaviour {
 		foreach (JSONObject psdlayer in json.list)
 		{
 			//TODO just pass JSON object and extra fields in CopyPixels
-			CopyPixels(sourceTex.width,sourceTex.height,(int)psdlayer["atlasx"].i,(int)psdlayer["atlasy"].i,(int)psdlayer["width"].i,(int)psdlayer["height"].i,(int)psdlayer["x"].i,(int)psdlayer["y"].i,(int)psdlayer["psdwidth"].i,(int)psdlayer["psdheight"].i,(int)psdlayer["z_order"].i);
+			CopyPixels(psdlayer, sourceTex.width,sourceTex.height,(int)psdlayer["atlasx"].i,(int)psdlayer["atlasy"].i,(int)psdlayer["width"].i,(int)psdlayer["height"].i,(int)psdlayer["x"].i,(int)psdlayer["y"].i,(int)psdlayer["psdwidth"].i,(int)psdlayer["psdheight"].i,(int)psdlayer["z_order"].i);
 		}
 	
 					
 	}
 	
 	
-	private void CopyPixels(int atlasWidth,int atlasHeight, int atlasX, int atlasY,int width,int height,int x,int y,int psdwidth, int psdheight, int z_order)
+	private void CopyPixels(JSONObject psdlayer, int atlasWidth,int atlasHeight, int atlasX, int atlasY,int width,int height,int x,int y,int psdwidth, int psdheight, int z_order)
 	{		
 		Color[] clearpix = transTex.GetPixels(0,0,psdwidth,psdheight);
 		Color[] pix = sourceTex.GetPixels(atlasX,(atlasHeight-atlasY-height),width,height);
@@ -40,7 +40,9 @@ public class Main : MonoBehaviour {
 		destTex.SetPixels(x,psdheight - y - height,width,height,pix);
 		destTex.Apply();
 		
-		GameObject go = new GameObject("psd - layer " + z_order);
+		string layerName = psdlayer["filename"].str;
+		
+		GameObject go = new GameObject(layerName + " - " + z_order);
 		go.AddComponent<SpriteRenderer>();
 		go.GetComponent<SpriteRenderer>().sprite =  Sprite.Create(destTex,new Rect(0,0,destTex.width,destTex.height),new Vector2(0,0));		
 		go.GetComponent<SpriteRenderer>().sortingOrder = 100 - z_order ;
